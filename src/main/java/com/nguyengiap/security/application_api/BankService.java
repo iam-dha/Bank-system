db4c677911bce9ebc719162b8ddbe490ce87eaa2
spad0604 <giap.nv225126@sis.hust.edu.vn>
2024-11-27 15:28:43 +0700

    Update Account

--- src/main/java/com/nguyengiap/security/application_api/BankService.java
+++ src/main/java/com/nguyengiap/security/application_api/BankService.java
@@ -7,7 +7,6 @@ import com.nguyengiap.security.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
