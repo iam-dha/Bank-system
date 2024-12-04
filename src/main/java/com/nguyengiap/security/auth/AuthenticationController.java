d7577e0a89ffc42669da35a382ed0a118ad6b106
spad0604 <giap.nv225126@sis.hust.edu.vn>
2024-12-04 22:49:53 +0700

    Check account exist

--- src/main/java/com/nguyengiap/security/auth/AuthenticationController.java
+++ src/main/java/com/nguyengiap/security/auth/AuthenticationController.java
@@ -2,22 +2,35 @@ package com.nguyengiap.security.auth;
 
 import com.nguyengiap.security.auth.model.request_model.AuthenticationRequest;
 import com.nguyengiap.security.auth.model.request_model.RegisterRequest;
+import com.nguyengiap.security.database_model.user.User;
+import com.nguyengiap.security.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
 
 
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
+    @Autowired
     private final AuthenticationService authenticationService;
 
+    @Autowired
+    private final UserService userService;
+
     @PostMapping("/register")
-    public ResponseEntity<AuthenticationResponse> register(
+    public ResponseEntity<?> register(
             @RequestBody RegisterRequest request
     ) {
+        Optional<User> user = userService.findByAccount(request.getAccount());
+
+        if(user.isPresent()) {
+            return ResponseEntity.status(403).body("Account is exist");
+        }
         return ResponseEntity.ok(authenticationService.register(request));
     }
 
