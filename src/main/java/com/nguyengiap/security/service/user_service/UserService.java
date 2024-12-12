65b41081d7a2c0aa7ae79b492e9092f66cad0216
spad0604 <giap.nv225126@sis.hust.edu.vn>
2024-12-12 10:29:15 +0700

    test

---
+++ src/main/java/com/nguyengiap/security/service/user_service/UserService.java
@@ -0,0 +1,52 @@
+package com.nguyengiap.security.service.user_service;
+
+import com.nguyengiap.security.database_model.user.User;
+import com.nguyengiap.security.database_model.user.UserRepository;
+import com.nguyengiap.security.model.response_model.BalanceWithAccount;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public Optional<User> findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    public Optional<User> findByAccount(String account) {
+        return userRepository.findByAccount(account);
+    }
+
+    public Optional<BalanceWithAccount> findBalanceByAccount(String account) {
+        return userRepository.findBalanceByAccount(account);
+    }
+
+    public void bankingToAccount(String account, Integer fund) {
+        userRepository.bankingToAccount(account, fund);
+    }
+
+    public void bankingToAccount2(String account, Integer fund) {
+        userRepository.bankingToAccount2(account, fund);
+    }
+
+    public void changePassword(String account, String password) {
+        userRepository.changePassword(account, password);
+    }
+
+    public void updateAddress(String account, String address) {
+        userRepository.updateAddress(account, address);
+    }
+
+    public void updateEmail(String account, String email) {
+        userRepository.updateEmail(account, email);
+    }
+
+    public void updatePhoneNumber(String account, String phoneNumber) {
+        userRepository.updatePhoneNumber(account, phoneNumber);
+    }
+}
