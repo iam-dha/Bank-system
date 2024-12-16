e49665e89a86fa7e1497758bdad2d505f06dfc78
spad0604 <giap.nv225126@sis.hust.edu.vn>
2024-12-17 00:11:35 +0700

    Không code nữa đâu

--- backend/src/main/java/com/nguyengiap/security/config/jwt_config/JwtService.java
+++ backend/src/main/java/com/nguyengiap/security/config/jwt_config/JwtService.java
@@ -48,7 +48,7 @@ public class JwtService {
                 .setClaims(extraClaims)
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 10))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 15))
                 .signWith(getSignInKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
